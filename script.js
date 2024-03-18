e18d09e69988ac2f495948f455a2aeb248af9a2f
Rohan3478 <158831788+Rohan3478@users.noreply.github.com>
2024-03-18 21:40:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  question.addEventListener('click', function() {
+    const answer = this.nextElementSibling; 
+
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      answer.style.display = 'block';
+    }
+  });
+});
